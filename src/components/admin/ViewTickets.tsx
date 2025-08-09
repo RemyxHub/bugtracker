6861519baffab92f2ca467d1feab7b57998efaf1
RemyxHub <remyx782@gmail.com>
2025-08-09 23:02:32 +0800

    Updated ViewTickets.tsx

--- src/components/admin/ViewTickets.tsx
+++ src/components/admin/ViewTickets.tsx
@@ -199,7 +199,7 @@ const ViewTickets = () => {
       case "assigned":
         return "bg-blue-100 text-blue-800 dark:bg-blue-900 dark:text-blue-300";
       case "cancelled":
-        return "bg-red-100 text-red-800 dark:bg-red-900 dark:text-red-300";
+        return "bg-red-100 text-red-800 dark:bgadmin@ryotek.my-red-900 dark:text-red-300";
       case "open":
       case "new":
         return "bg-yellow-100 text-yellow-800 dark:bg-yellow-900 dark:text-yellow-300";
