28562966cf26e77d6aeb17d076e49658b2279642
RemyxHub <remyx782@gmail.com>
2025-08-28 13:37:20 +0800

    Updated TicketManagement.tsx

--- src/components/admin/TicketManagement.tsx
+++ src/components/admin/TicketManagement.tsx
@@ -520,7 +520,7 @@ const TicketManagement = () => {
                               {selectedTicket?.title}
                             </DialogDescription>
                             <DialogDescription>
-                              Assigned to : {selectedStaff.name}
+                              Assigned to : {selectedStaff.id}
                             </DialogDescription>
                           </DialogHeader>
 
